20c21dab1be26087872bcfe024a34992ec2556e5
Наталия <105249744+NataliyaChyi@users.noreply.github.com>
2022-07-23 17:07:41 +0300

    connection openweathermap

--- WeatherApp/weather/views.py
+++ WeatherApp/weather/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
+import requests
 
 def index(request):
+    appid = '8b4995461704791e59ced235a0081d95'
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid="+appid
+    city = "London"
     return render(request, 'weather/index.html')
-
-def new_address(request):
-    return render(request, 'weather/new_address.html')
-
