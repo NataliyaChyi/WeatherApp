1d33da764d579e7fe7f1a3cf3ecae7b6055d2102
Наталия <105249744+NataliyaChyi@users.noreply.github.com>
2022-07-23 18:13:26 +0300

    the weather in Minsk

--- WeatherApp/weather/views.py
+++ WeatherApp/weather/views.py
@@ -3,6 +3,8 @@ import requests
 
 def index(request):
     appid = '8b4995461704791e59ced235a0081d95'
-    url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid="+appid
-    city = "London"
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid="+appid
+    city = "Minsk"
+    res = requests.get(url.format(city)).json()
+    print(res)
     return render(request, 'weather/index.html')
